1a16c13879327ada88c7e6851b947559546a8269
Ekscursionist <larakek@gmail.com>
2018-10-01 15:31:47 -0400

    I don't know what I'm doing so I'm going to try this again tomorrow

--- hw15/src/App.js
+++ hw15/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import './App.css';
+import LeafCard from "./components/LeafCard";
+import Wrapper from "./components/Wrapper";
+import TopText from "./components/TopText";
 import leafs from "./leafs.json";
+import './App.css';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+  state= {
+    message: "click a leaf to start!",
+    leafs,
+    score: 0,
+    highscore: 0
   }
+
+  componentDidMount() {
+  }
+
+  shufflecards = array => {
+    for (let i = array.length -1; i > 0; i--) {
+      let z = Math.floor(Math.random() * (i + 1));
+      [array[i], array[z]] = [array[z], array[i]]
+    }
+  }
+
+
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
+
 }
 
 export default App;
--- hw15/src/components/LeafCard/LeafCard.js
+++ hw15/src/components/LeafCard/LeafCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./leafcard.css";
 
-const leafcard = props => (
+const LeafCard = props => (
     <div className="card">
         <div className="img-container">
             <a onClick={() => props.selectLeaf(props.type)} className={props.currentscore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit" } >
@@ -11,4 +11,4 @@ const leafcard = props => (
     </div>
 );
 
-export default leafcard;
+export default LeafCard;
