8818470ea07dbe7494c247538d4d1ad04041061c
Ekscursionist <larakek@gmail.com>
2018-10-01 14:41:49 -0400

    creating LeafCard, incl hover, shake, etc.

--- hw15/src/App.js
+++ hw15/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import leafs from "./leafs.json";
 
 class App extends Component {
   render() {
---
+++ hw15/src/components/LeafCard/LeafCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./leafcard.css";
+
+const leafcard = props => (
+    <div className="card">
+        <div className="img-container">
+            <a onClick={() => props.selectLeaf(props.type)} className={props.currentscore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit" } >
+                <img alt={ props.type } src={ props.image } />
+            </a>
+        </div>
+    </div>
+);
+
+export default leafcard;
