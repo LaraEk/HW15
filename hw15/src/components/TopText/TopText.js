822c0be412e04358c2a0f3eee288704f0be8a631
Ekscursionist <larakek@gmail.com>
2018-10-01 14:49:13 -0400

    created TopText and its prelim CSS; downloaded bckgd pattern

---
+++ hw15/src/components/TopText/TopText.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./toptext.css";
+
+const TopText = props => (
+    <div className="textybox">
+        <h1>Collect Fall Leaves!</h1> 
+        <p>Click on the leaves to rake them up!  But the trees will get angry if you click hte same leaf twice!</p>
+    </div>
+);
+
+export default TopText;
