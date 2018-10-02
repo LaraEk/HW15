3e5394b4769d864234ce710e631567119362cd86
Ekscursionist <larakek@gmail.com>
2018-10-02 15:01:40 -0400

    added IDs to leafs.json; writing the shuffle function in app.js

--- hw15/src/App.js
+++ hw15/src/App.js
@@ -11,35 +11,44 @@ class App extends Component {
   state= {
     message: "click a leaf to start!",
     leafs,
+    thechosenleafs = [],
     score: 0,
     highscore: 0
-  }
+  };
 
-  componentDidMount() {
-  }
+  // this is the function to shuffle when clicked!
+  shuffleLeafs = id => {
+    let clickedLeafIDs = this.state.clickedLeafIDs;
 
-  shufflecards = array => {
-    for (let i = array.length -1; i > 0; i--) {
-      let z = Math.floor(Math.random() * (i + 1));
-      [array[i], array[z]] = [array[z], array[i]]
-    }
-  }
+    if(clickedLeafIDs.includes(id)) {
+      this.setState({ clickedLeafIDs: [], score: 0, status: "Your leaf-bag has been emptied! Keep clicking to start anew!" });
+      console.log("loss condition achieved");
+      return;
+    } else {
+      clickedLeafIDs.push(id)
+
+      if (clickedLeafIDs.length === 8) {
+        this.setState({ score: 8 status: "You have gatehred all the leaves!  Your yard is clean! Keep clicking to help clear your neighbor's yard...", clickedLeafIDs: [] });
+        console.log("win condition achieved");
+        return;
+      }
+
+      this.setState({ leafs, clickedLeafIDs, score: clickedLeafIDs.length, status: " " });
+
+      for(let i = leafs.length -1; i > 0; i--) {
+        let z = Math.floor(Math.random() * (i + 1));
+        [leafs[i], leafs[z] = leafs[z], leafs[i]];
+        console.log("shuffle shuffle");
+      }
+
+    } // this curly is to ELSE above
+
+  } // this curly finished the SHUFFLE LEAFS function
 
 
 
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <h1 className="App-title">Welcome to React</h1>
-  //       </header>
-  //       <p className="App-intro">
-  //         To get started, edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //     </div>
-  //   );
-  // }
 
+  
 }
 
 export default App;
