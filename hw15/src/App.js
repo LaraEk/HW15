f01b214c484758bd70e7dd5db3ceb35564db6eba
Ekscursionist <larakek@gmail.com>
2018-10-18 20:26:23 -0400

    FINISHED!

--- hw15/src/App.js
+++ hw15/src/App.js
@@ -42,53 +42,16 @@ class App extends Component {
         score: 0,
         clickedLeafIDs: [],        
       });
-    } else if (this.state.clickedLeafIDs.length === 7) {
+    } else if (this.state.clickedLeafIDs.length === 2) {
       console.log("win condition achieved!")
       this.setState({
-        
+        status: "You have gathered all the leaves!  Your yard is clean! Keep clicking to help clear your neighbor's yard...",
+        score: 0,
+        clickedLeafIDs: [],        
       });
     }
 
-//         this.setState({ score: 8, status: "You have gathered all the leaves!  Your yard is clean! Keep clicking to help clear your neighbor's yard...", clickedLeafIDs: [] });
-//         console.log("win condition achieved");
-//         return;
-//       }
-
-
-
   } // this curly finished the SHUFFLE LEAFS function
-
-
-//     if(this.state.clickedLeafIDs.includes(id)) {
-//       this.setState({ clickedLeafIDs: [] });          // note: WHY DO SCORE AND STATUS NEVER LOG 
-//       this.setState({ score: 0 });
-//       this.setState({ status: "Your leaf-bag has been emptied! Keep clicking to start anew!" });
-//       console.log("loss condition achieved");
-//         return;
-//     } else {
-
-//       let a = this.state.clickedLeafIDs.slice(); //creates the clone of the state
-//       a[this.state.clickedLeafIDs.length - 1] = id;
-//       this.setState({clickedLeafIDs: a});
-//       // NOTE: if I X this part out, the array gets super-wonky.  why???  what does the clone help
-
-//       if (this.state.clickedLeafIDs.length === 8) {          // note: WHY DO SCORE AND STATUS NEVER LOG
-//         this.setState({ score: 8, status: "You have gathered all the leaves!  Your yard is clean! Keep clicking to help clear your neighbor's yard...", clickedLeafIDs: [] });
-//         console.log("win condition achieved");
-//         return;
-//       }
-
-// //      this.setState({ score: " " });
-      
-//       for(let i = leafs.length -1; i > 0; i--) {
-//         let z = Math.floor(Math.random() * (i + 1));
-//         [leafs[i], leafs[z]] = [leafs[z], leafs[i]];
-//         console.log("shuffle shuffle");
-//       }
-
-//       this.setState({ leafs: leafs });
-
-//     } // this curly is to ELSE above
 
 
 // Render Unto Class Component That Which Is Class Component's
